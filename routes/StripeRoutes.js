7fe410e1e965de54ec8d3cc3fb4fd9a214a63a91
Olawumi Olusegun <olawumi.olusegun@gmail.com>
2023-06-09 08:05:09 +0100

    Updated 029

--- routes/StripeRoutes.js
+++ routes/StripeRoutes.js
@@ -24,7 +24,9 @@ const { permissions } = require('./../middlewares/permissionsMiddleware');
 
 
 
-router.post('/api/stripe/webhook', express.raw({ type:"application/json"}), hooks);
+router.post('/api/stripe/webhook',  express.raw({ type: 'application/json' }), hooks);
+
+// 
 // router.route('/').post(hooks);
 // stripe listen --forward-to localhost:2000/api/stripe/webhook
 
